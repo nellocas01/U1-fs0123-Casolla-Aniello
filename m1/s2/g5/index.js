faaf7d76ff83232dd44799624ceb15ec3f207dd6
Nello <nellocasolla446@gmail.com>
2023-02-10 11:14:42 +0100

    first commit

---
+++ m1/s2/g5/index.js
@@ -0,0 +1,13 @@
+function aggiungiSimbolo(elemento){
+    //let simbolo = elemento.textContent;
+    let simbolo = elemento.getAttribute('data-simbolo');
+
+    let display = document.querySelector('#display');
+    display.value += simbolo;
+}
+
+function totale(){
+    let display = document.querySelector('#display');
+
+    display.value = eval(display.value);
+}
