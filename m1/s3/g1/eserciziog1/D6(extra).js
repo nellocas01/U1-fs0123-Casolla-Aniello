272850e5b26cafaa252a1d925a633c5e0a0d6a4e
Nello <nellocasolla446@gmail.com>
2023-02-13 17:01:43 +0100

    fist commit extra

--- m1/s3/g1/eserciziog1/D6(extra).js
+++ m1/s3/g1/eserciziog1/D6(extra).js
@@ -13,15 +13,45 @@
  Crea una funzione chiamata "shoppingCartTotal" che calcola il totale dovuto al negozio (tenendo conto delle quantità di ogni oggetto).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* SCRIVI QUI LA TUA RISPOSTA 
+var shoppingCart = {
+    price: 6,
+    name: "jeans",
+    id: "skinny",
+    quantity: 20
+,shoppingCart2 : {
+    price: 4,
+    name: "jeans",
+    id: "baggy",
+    quantity: 10
+} , shoppingCart3 :{
 
+}
+}
+function shoppingCartTotal(){
+    var tot = shoppingCart.price * shoppingCart.quantity;
+    var tot1 = shoppingCart2.price * shoppingCart2.quantity;
+    var tot2= tot + tot1;
+    console.log(tot2 + "€");
+} 
+shoppingCartTotal();*/
 /* EXTRA 3
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "addToShoppingCart" che riceve un nuovo oggetto dello stesso tipo, lo aggiunge a "shoppingCart" e ritorna il nuovo numero totale degli elementi.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-
+/* SCRIVI QUI LA TUA RISPOSTA 
+function addToShoppingCart(){
+   var shoppingCart3 = {
+    price2: 9,
+    name2: "jeans",
+    id2: "long",
+    quantity2: 30
+   }
+   shoppingCart.push(shoppingCart3);
+   console.log(shoppingCart);
+}
+addToShoppingCart();*/
 /* EXTRA 4
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "maxShoppingCart" che riceve l'array "shoppingCart" e ritorna l'oggetto più costoso in esso contenuto.
@@ -41,33 +71,60 @@
  La funzione è composta da un ciclo che stampa un numero casuale tra 0 e 9 finchè il numero casuale non è maggiore di x per tre volte di fila.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-
+/* SCRIVI QUI LA TUA RISPOSTA 
+function loopUntil(n = 7){
+    for(var i = 0; i < 10; i++){
+        i = Math.floor(Math.random()*10);
+    }console.log(i);
+}
+loopUntil();*/
 /* EXTRA 7
 Crea una funzione chiamata "average" che riceve un array come parametro e ne ritorna la media aritmetica. La funzione salta automaticamente i valori non numerici nell'array.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+function average(myArray) {
+    var i = 0, summ = 0, ArrayLen = myArray.length;
+    while (i < ArrayLen) {
+        summ = summ + myArray[i++];
+}
+return summ / ArrayLen;
+}
+var myArray = [1, 5, 2, 3, 9];
+var a = average(myArray);
+console.log(a)
 /* EXTRA 8
  Crea una funzione chiamata "longest" che trova la stringa più lunga all'interno di un array di stringhe fornito come parametro.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function longest(myStrArray){
 
+}
+longest();
 /* EXTRA 9
  Crea una funzione per creare un filtro anti-spam per la tua casella email. La funzione riceve un parametro stringa chiamato "emailContent", e torna un valore booleano.
  La funzione deve ritornare true se "emailContent" non contiene le parole "SPAM" o "SCAM".
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+function anti(s = "emailContent"){
+    if(s.includes("SPAM")|| s.includes("SCAM")){
+        console.log(false);
+    }else{
+        console.log(true);
+    }
+}
+anti();
 /* EXTRA 10
  Scrivi una funzione che riceve una data come parametro, e calcola il numero di giorni passati da quella data.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function data(){
 
+}
+data();
 /* EXTRA 11
  Scrivi una funzione chiamata "matrixGenerator" che riceve come paremetri due numeri interi, "x" e "y".
  Il risultato deve essere una matrice di "x" volte "y", e i valori devono rispecchiare gli indici della posizione all'interno della matrice.
@@ -77,3 +134,7 @@ Crea una funzione chiamata "average" che riceve un array come parametro e ne rit
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function matrixGenerator(x,y){
+
+}
+matrixGenerator();
