12ea7d18df3e35aa6f24c4555325a472c432beb6
Nello <nellocasolla446@gmail.com>
2023-02-15 16:50:07 +0100

    last commit

--- m1/s3/g3/esercizioG3/index.js
+++ m1/s3/g3/esercizioG3/index.js
@@ -1,2 +1,36 @@
-let input = document.getElementById('#input');
-let button = document.getElementById('#input-button')
+let input = document.getElementById('input');
+let button = document.getElementById('input-button');
+let taskList = document.getElementById('task-list');
+
+button.addEventListener('click', createList);
+
+function createList(){
+
+    if(input.value === ""){
+        alert("scrivi qualcosa")
+    }
+
+    let li = document.createElement('li');
+    let del = document.createElement('button');
+
+    li.textContent = input.value;
+    input.value = " ";
+    del.textContent = 'X';
+    li.append(del);
+    taskList.append(li);
+    
+}
+
+
+
+    let buttonDelete = document.querySelectorAll('#task-list button');
+
+    for(button of buttonDelete){
+    buttonDelete.addEventListener('click', () => {
+
+        
+        li.remove();
+        
+    });
+
+}
